f115cc724778cd8c42ed191936876d907a140f47
douyouzhe <douyouzhe@gmail.com>
2017-09-24 16:19:02 -0400

    1

--- CV_HW1/HW1.py
+++ CV_HW1/HW1.py
@@ -44,40 +44,6 @@ def p1(gray_in, thresh_val): # return binary_out
 
 
 
-
-#####
-class DisjointSet(object):
-
-    def __init__(self):
-        self.leader = {} # maps a member to the group's leader
-        self.group = {} # maps a group leader to the group (which is a set)
-
-    def add(self, a, b):
-        leadera = self.leader.get(a)
-        leaderb = self.leader.get(b)
-        if leadera is not None:
-            if leaderb is not None:
-                if leadera == leaderb: return # nothing to do
-                groupa = self.group[leadera]
-                groupb = self.group[leaderb]
-                if len(groupa) < len(groupb):
-                    a, leadera, groupa, b, leaderb, groupb = b, leaderb, groupb, a, leadera, groupa
-                groupa |= groupb
-                del self.group[leaderb]
-                for k in groupb:
-                    self.leader[k] = leadera
-            else:
-                self.group[leadera].add(b)
-                self.leader[b] = leadera
-        else:
-            if leaderb is not None:
-                self.group[leaderb].add(a)
-                self.leader[a] = leaderb
-            else:
-                self.leader[a] = self.leader[b] = a
-                self.group[a] = set([a, b])
-#####
-
 def p2(binary_in):  # return labels_out
     image = cv2.imread(binary_in, 0)
     shape = image.shape
@@ -88,85 +54,63 @@ def p2(binary_in):  # return labels_out
 
     for i in range(shape[0]): # 1 is white o is background
         for j in range(shape[1]):
-            if image[i,j] == 0: #background
-                a[i,j] = 0;
+            if image[i,j] == 0:
+                a[i,j] = 0
             elif image[i,j] == 255 and a[i-1,j-1] != 0: #D is labeled
                 a[i,j] = a[i-1,j-1];
             elif image[i,j] == 255 and a[i-1,j] == 0 and a[i,j-1] == 0: #B and C are not labeled
-                a[i,j] = k;
-                k+=1; #update label
+                a[i,j] = k
+                k+=1
             elif image[i,j] == 255 and a[i-1,j] !=0 and a[i,j-1] ==0: #B is labeled
-                a[i,j] = a[i-1,j];
+                a[i,j] = a[i-1,j]
             elif image[i,j] == 255 and a[i,j-1] !=0 and a[i-1,j] ==0: #C is labeled
-                a[i,j] = a[i,j-1];
+                a[i,j] = a[i,j-1]
             elif a[i,j-1] !=0 and a[i-1,j] !=0: #B and C are labeled
                 if a[i,j-1] ==  a[i-1,j] :
-                    a[i,j] = a[i,j-1];
+                    a[i,j] = a[i,j-1]
                 else:
-                    a[i,j] = a[i,j-1];
-                    b.append(a[i-1,j]);
-                    c.append(a[i,j-1]);
-    k=0; h=0
-    b_t = np.array(b)
+                    a[i,j] = a[i,j-1]
+                    b.append(a[i-1,j])
+                    c.append(a[i,j-1])
 
     dic = {}
-    dic2 = {}
-    group = 0
+    group = 100
     ls=[]
-
     for k in range(len(b)):
         ls.append([b[k],c[k]])
-
     ls.sort()
 
-
-
     for k in range(len(ls)):  # Create Eq_Table
+        if (ls[k][0] in dic) and (ls[k][1] in dic):
+            m = dic.get(ls[k][0])
+            n = dic.get(ls[k][1])
+            if(m<n):
+                for i in dic:
+                    if dic.get(i)==n:
+                        dic[i]=m
+            else:
+                for i in dic:
+                    if dic.get(i)==m:
+                        dic[i]=n
         if ls[k][0] in dic:
             dic[ls[k][1]] = dic.get(ls[k][0])
-        elif ls[k][1] in dic:
+        if ls[k][1] in dic:
             dic[ls[k][0]] = dic.get(ls[k][1])
         else:
-            group = group + 1
+            group = group + 100
             dic[ls[k][0]] = group
             dic[ls[k][1]] = group
-            #print k
+    row,col = a.shape
+    pic = np.zeros([row,col])
+    for i in range(0, row):
+        for j in range(0, col):
+            if(a[i,j]!=0):
+                pic[i,j] = dic.get(a[i,j])
 
-    # c.sort()
-    # b.sort()
-    # for k in range(len(b)):  # Create Eq_Table
-    #     if b[k] in dic:
-    #         dic[c[k]] = dic.get(b[k])
-    #     elif c[k] in dic:
-    #         dic[b[k]] = dic.get(c[k])
-    #     else:
-    #         group = group + 1
-    #         dic[b[k]] = group
-    #         dic[c[k]] = group
-
-    # print b;
-    # print c;
-    # for i in dic.values():
-    #     if i==3:
-    #         print "a"
-    #
-    # print ls[103]
-    # print ls[104]
-    # print ls[105]
-
-    #print dic.values()
-    aa=[]
-    for [m,n] in ls:
-        if(dic.get(m)==1):
-            aa.append(m)
-        if(dic.get(n)==1):
-            aa.append(n)
-
-    print aa
-    return a;
+    return pic
 
 ###### test p2 #######
 pic = p2('binaryImage.pgm')
 #cv2.imshow('',pic)
 #cv2.waitKey(0)
-#cv2.imwrite("newLabel.pgm", pic)
+cv2.imwrite("newLabel.pgm", pic)
