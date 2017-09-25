0203fbafc17bdc8a93ca7968f256e2572237c71f
douyouzhe <douyouzhe@gmail.com>
2017-09-25 13:57:21 -0400

    1

--- CV_HW1/P2.py
+++ CV_HW1/P2.py
@@ -20,15 +20,15 @@ def p2(binary_in):  # return labels_out
     image = cv2.imread(binary_in, 0)
     shape = image.shape
     a = np.zeros(shape, int)
-    label=1
+    label=1 # label before equivalence
     dic = {}
-    group = 0
+    group = 0 # label after equivalence
     ls=[]
 
-    for i in range(shape[0]): # 1 is white o is background
+    for i in range(shape[0]): # 255 is white 0 is background
         for j in range(shape[1]):
             if image[i,j] == 0:
-                a[i,j] = 0
+                a[i,j] = 0 # pass background
             elif a[i-1,j-1] != 0: #D is labeled
                 a[i,j] = a[i-1,j-1]
             elif a[i-1,j] == 0 and a[i,j-1] == 0: #B and C are not labeled
@@ -43,10 +43,12 @@ def p2(binary_in):  # return labels_out
                     a[i,j] = a[i,j-1]
                 else:
                     a[i,j] = a[i,j-1]
-                    ls.append([a[i-1,j],a[i,j-1]])
-    ls.sort()
-    for k in range(len(ls)):  # Create Eq_Table
+                    ls.append([a[i-1,j],a[i,j-1]]) # save paired labels
+    ls.sort()#unnucessary in this case
+    for k in range(len(ls)):  # Create Equivalence Table
         if (ls[k][0] in dic) and (ls[k][1] in dic):
+            #if both labels are in dic, change the label to the smaller one
+            #need to change all previous labels as well
             m = dic.get(ls[k][0])
             n = dic.get(ls[k][1])
             if(m<n):
@@ -57,16 +59,19 @@ def p2(binary_in):  # return labels_out
                 for i in dic:
                     if dic.get(i)==m:
                         dic[i]=n
+        # if either one not in dic, add the same label to them
         if ls[k][0] in dic:
             dic[ls[k][1]] = dic.get(ls[k][0])
         if ls[k][1] in dic:
             dic[ls[k][0]] = dic.get(ls[k][1])
+        #both not found, create new label
         else:
             group = group + 100
             dic[ls[k][0]] = group
             dic[ls[k][1]] = group
     row,col = a.shape
     pic = np.zeros([row,col])
+    # give labels a specific value so they can be seen
     for i in range(0, row):
         for j in range(0, col):
             if(a[i,j]!=0):
